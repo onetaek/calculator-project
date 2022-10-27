f0ffa679d4c2b6f9709842cd1c982b805803bf81
oh one teak <dnjsxorchlrh@naver.com>
2022-10-28 01:55:09 +0800

    css shadow 수정

--- js/common.js
+++ js/common.js
@@ -6,6 +6,8 @@ for(let i = 0 ; i < btn.length ; i++){
     btn[i].addEventListener('click',function(){
       if(this.innerHTML === "="){
         value.innerHTML = eval(value.innerHTML);
+        console.log(eval(value.innerHTML));
+        console.log(typeof(eval(value.innerHTML)));
       }else{
         if(this.innerHTML == "Clear"){
             value.innerHTML ="";
